d481bbc196b24b882ba19a3c94865039a612e022
Tien Nguyen <nqtien310@gmail.com>
2015-05-16 19:04:17 +0700

    Prototype

--- dist/carousel.js
+++ dist/carousel.js
@@ -12,34 +12,34 @@ define(["jquery"], function(){
     this.direction  = opts.direction
     this.$container = $(this.container)
     this.$children  = this.$container.find(" > div")
+  }
 
-    this.start = function(){
-      var self = this;
-      this.$children.first().addClass("carousel-active")
+  Carousel.prototype.start = function(){
+    var self = this;
+    this.$children.first().addClass("carousel-active")
 
-      setInterval(function(){
-        self.slide()
-      }, this.delayInSec * 1010)
-    }
+    setInterval(function(){
+      self.slide()
+    }, this.delayInSec * 1010)
+  }
 
-    this.slide = function(){
-      var $activeChild = this.$container.find(".carousel-active")
-      var $nextActiveChild = $activeChild.nextOrFirst("div")
-      var className = this.getDirectionClass()
-      $activeChild.addClass("animation " + className)
-      $nextActiveChild.addClass("next-carousel-active")
-      setTimeout(function(){
-        $activeChild.removeClass("animation carousel-active " + className)
-        $nextActiveChild.toggleClass("carousel-active next-carousel-active")
-      }, this.delayInSec * 1000)
-    }
+  Carousel.prototype.slide = function(){
+    var $activeChild = this.$container.find(".carousel-active")
+    var $nextActiveChild = $activeChild.nextOrFirst("div")
+    var className = this.getDirectionClass()
+    $activeChild.addClass("animation " + className)
+    $nextActiveChild.addClass("next-carousel-active")
+    setTimeout(function(){
+      $activeChild.removeClass("animation carousel-active " + className)
+      $nextActiveChild.toggleClass("carousel-active next-carousel-active")
+    }, this.delayInSec * 1000)
+  }
 
-    this.getDirectionClass = function(){
-      if(this.direction == "random"){
-        return "moved-" + ["left", "right", "top", "bottom"][Math.floor(Math.random() * 4)]
-      }else{
-        return "moved-" + this.direction
-      }
+  Carousel.prototype.getDirectionClass = function(){
+    if(this.direction == "random"){
+      return "moved-" + ["left", "right", "top", "bottom"][Math.floor(Math.random() * 4)]
+    }else{
+      return "moved-" + this.direction
     }
   }
 
